fe3be7544ce40aa17301e3c9c2d3a9206764a3f7
Shivesh <159266089+24Shivesh@users.noreply.github.com>
2025-07-09 02:26:41 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDgF7Ragam9FkfPJCYF_k8PkdZllPwU_Cw",
-  authDomain: "todo-app-c3357.firebaseapp.com",
-  projectId: "todo-app-c3357",
-  storageBucket: "todo-app-c3357.firebasestorage.app",
-  messagingSenderId: "321369835062",
-  appId: "1:321369835062:web:3fbb4852b5fed3173cba97",
-  measurementId: "G-03EFNYVBQ0"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
